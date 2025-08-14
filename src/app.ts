eec2988115bf9c03aeac6691e6a0da6921178179
Gertjan <gertjan@snok.nl>
2025-08-14 15:32:09 +0200

    feat: added HTTPS support

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import fs from "fs";
+import dotenv from 'dotenv';
+import http from "http";
+import https from "https";
 import { getToken } from "./controllers/authController";
 import { authenticate } from "./middlewares/authMiddleware";
 import * as v2 from "./controllers/v2";
 import * as v3 from "./controllers/v3";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 
@@ -43,8 +50,16 @@ app.use((req, res) => {
   });
 });
 
+// SSL configuration
+const cert = process.env.SSL_CERTIFICATE ?? (process.env.SSL_CERTIFICATE_FILE ? fs.readFileSync(process.env.SSL_CERTIFICATE_FILE as string) : undefined);
+const key = process.env.SSL_KEY ?? (process.env.SSL_KEY_FILE ? fs.readFileSync(process.env.SSL_KEY_FILE as string) : undefined);
+
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+if (!cert && !key) {
+  console.warn("No SSL certificate or key provided. Running in HTTP mode.");
+}
+var server = (!cert || !key) ? http.createServer(app) : https.createServer({ key, cert }, app);
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
